9e9847306ca6f1566f36f863670733f835da83c0
Jack Pooley <jackpooleybc@users.noreply.github.com>
2021-08-04 14:16:04 +0200

    Fix Platforms declaration in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "RxCombine",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RxCombine",
             targets: ["RxCombine"]),
-    ],
-    platforms: [
-      .iOS(.14)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
